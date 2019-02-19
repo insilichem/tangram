9db6cf98747a994e02189bdeeb16439caa4d2716
Jaime RGP <jaime.rogue@gmail.com>
2019-02-19 12:31:28 +0100

    docs: update version in conf.py [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ author = u'Jaime Rodríguez-Guerra, InsiliChem'
 # built documents.
 #
 # The short X.Y version.
-version = u'0.0.3'
+version = u'0.0.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
